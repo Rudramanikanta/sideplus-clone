21ae890918171303bcc8f884156f3ac7bf6ffb04
rudramanikanta <rudramanikanta02@gmail.com>
2024-09-15 12:26:24 +0530

    optimized

--- sideplus/app/components/Second.js
+++ sideplus/app/components/Second.js
@@ -1,45 +1,59 @@
-"use client"
-import React, { useEffect } from 'react'
-import { useInView } from 'react-intersection-observer'
-import { motion, useScroll, useTransform } from 'framer-motion'
-import Image from 'next/image'
+"use client";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
 
-const Second = ({images,prev,buttonText,image}) => {
-    const animation2={
-        hidden:{y:-100,opacity:0},
-        visible:{y:0,opacity:100}
-    }
-    const { ref, inView } = useInView({
-        triggerOnce: true,
-    })
+const Second = ({ images, prev, buttonText, image, side }) => {
+ 
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
 
-    
-    
-    
-    const animation = {
-        hidden: { y: 100, opacity: 0 },
-        visible: { y: 0, opacity: 1 },
-    }
+  const animation = {
+    hidden: { y: 100, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+  };
 
-    
-    return (
-        <motion.div className='mb-24'  ref={ref} variants={animation} initial="hidden" animate={inView? "visible" : "hidden"}>
-            <div className='flex justify-between p-10'>
-                <div className='items-center content-center md:flex md:align-middle'>
-                    <Image src={image}  alt="Series banner" className='h-[100px] w-[150px] object-contain'/>
-                    <p className='ml-10'>Exclusive series just for sideplus+ members</p>
-                </div>
-                <button className='px-4 py-4 rounded-md bg-neutral-400/10 h-fit w-fit'>{buttonText}</button>
-            </div>
-            <div className='grid grid-cols-2 px-10 lg:grid-cols-4 gap-x-4 '>
-                {images.map((val, index) => (
-                    <div key={index} style={{ backgroundImage: `url(${val.img})` }} className="relative  group block bg-center bg-cover h-[400px] rounded-lg overflow-hidden  item hover:bg-opacity-50 ">
-                        <motion.button variants={animation2} initial="hidden" animate="visible" className='absolute left-[-4] ml-[-10] hidden px-3 font-semibold text-black bg-white rounded-md group-hover:block bottom-10 '><p className='ml-10'>{val.button}</p></motion.button>
-                    </div>
-                ))}
-            </div>
-        </motion.div>
-    )
-}
+  return (
+    <motion.div
+      className="mb-24"
+      ref={ref}
+      variants={animation}
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+    >
+      <div className="flex justify-between p-10">
+        <div className="items-center content-center md:flex md:align-middle">
+          <Image
+            src={image}
+            alt="Series banner"
+            className="h-[100px] w-[150px] object-contain "
+          />
+          <p className="ml-10">{side}</p>
+        </div>
+        <button className="px-4 py-4 rounded-md bg-neutral-400/10 h-fit w-fit">
+          {buttonText}
+        </button>
+      </div>
+      <div className="grid grid-cols-2 px-10 lg:grid-cols-4 gap-x-4 ">
+        {images.map((val, index) => (
+          <div
+          key={index}
+          style={{ backgroundImage: `url(${val.img})` }}
+          className="relative group block bg-center h-[380px] rounded-lg overflow-hidden item bg-no-repeat hover:bg-neutral-400"
+        >
+          <button
+            className="absolute flex px-3 font-bold text-black transition-all duration-1000 ease-in transform bg-white opacity-0 group-hover:opacity-100 group-hover:translate-y-0 rounded-r-md bottom-10 -translate-y-1/4"
+          >
+            <p className="ml-6 font-extrabold text-md">{val.button}</p>
+          </button>
+        </div>
+        
+        ))}
+      </div>
+    </motion.div>
+  );
+};
 
-export default Second
+export default Second;
--- sideplus/app/home/page.js
+++ sideplus/app/home/page.js
@@ -10,51 +10,87 @@ import { motion } from "framer-motion";
 import { useScroll, useTransform } from "framer-motion";
 import { useEffect, useRef } from "react";
 import Second from "../components/Second";
-import image from "./image.png"
-import BTS from "./BTS.svg"
-import Third from "./third.svg"
+import image from "./image.png";
+import BTS from "./BTS.svg";
+import Third from "./third.svg";
 const page = () => {
   const router = useRouter();
   const session = useSession();
   if (session.data == null) {
     router.push("/");
   }
-  const big=useRef(null)
-  const {scrollYProgress}=useScroll({
-    target:big,
-    offset:["start end","end start"]
-  })
+  console.log(session);
+  const big = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: big,
+    offset: ["start end", "end start"],
+  });
   const images = [
     {
-        button: "GAME SHOWS",
-        img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/6659ac5b8c9fa4ed8d90e4fe_Game-Covers-Banner-.webp"
+      button: "GAME SHOWS",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/6659ac5b8c9fa4ed8d90e4fe_Game-Covers-Banner-.webp",
     },
     {
-        button: "THE SIDECAST",
-        img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/645a242382a2fe03e3463e37_SIDECAST-POSTER.webp"
+      button: "THE SIDECAST",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/645a242382a2fe03e3463e37_SIDECAST-POSTER.webp",
     },
     {
-        button: "GUESTCAST",
-        img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/645a2439b999bb95fc2e51e0_GUESTCAST-POSTER.webp"
+      button: "GUESTCAST",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/645a2439b999bb95fc2e51e0_GUESTCAST-POSTER.webp",
     },
     {
-        button: "SIDE+ SATURDAYS",
-        img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/66757e4102128f46509a7c73_65dc98e1670dd62f61a78684_Side%2B-Saturdays-Banner1.jpg"
-    }
-]
-const ref=useRef(null);
-const mar = useScroll({
-  target: ref,
-  offset: ["start end", "start start"],
-})
-const  padding=useTransform(mar?.scrollYProgress, [0, 1], ["100px", "-80px"]);
+      button: "SIDE+ SATURDAYS",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/66757e4102128f46509a7c73_65dc98e1670dd62f61a78684_Side%2B-Saturdays-Banner1.jpg",
+    },
+  ];
+  const images2 = [
+    {
+      button: "ROAST BTS",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/645a283a0c7f639156aa4ebe_Roast_of_the%20_sidemen.webp",
+    },
+    {
+      button: "$100 vs $10,000 BTS",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/645a2869b1013f67e4921724_Sidemen%20100_v10000.webp",
+    },
+    {
+      button: "JAPAN BTS",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/645a2439b999bb95fc2e51e0_GUESTCAST-POSTER.webp",
+    },
+    {
+      button: "WEIRD HOTELS BTS",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/645a284497a7dca94d0dffc6_sidemen_weird_hotels.webp",
+    },
+  ];
+  const images3 = [
+    {
+      button: "IPHONE",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/654bcb0ec547cbd6354717b2_IMG_4144.JPG",
+    },
+    {
+      button: "PLAYSTATION 5",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/66757abb6fd2437aa980e80c_64626e49fe447883cc20d054_PS5%20standard.webp",
+    },
+    {
+      button: "NINTENDO SWITCH",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/66757b5e2a203b464e6e67e3_nintendo%20switch%20side%2B.jpeg",
+    },
+    {
+      button: "OSMO POCKET VLOG",
+      img: "https://cdn.prod.website-files.com/610c5f70ab032078a5ff059f/66954b425890d3c3627fe776_DJI-Osmo-Thumnail.png",
+    },
+  ];
+  const ref = useRef(null);
+  const mar = useScroll({
+    target: ref,
+    offset: ["start end", "start start"],
+  });
+  const padding = useTransform(
+    mar?.scrollYProgress,
+    [0, 1],
+    ["100px", "-80px"]
+  );
 
-useEffect(()=>{
-  mar.scrollYProgress.onChange((val)=>{
-    console.log(val)
-  })
-})
-  const scale=useTransform(scrollYProgress,[0,1],[1,1.3])
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 1.3]);
   return (
     <div className="bg-[#171717]">
       <div className="bg-[url('https://cdn.prod.website-files.com/60f1486c9971edb7304fc4f5/63d3c74f2c8dce26ba36947e_1673614238746.webp')] h-screen bg-center">
@@ -69,6 +105,9 @@ useEffect(()=>{
               className="p-2 border-black-100"
               onClick={() => {
                 signOut();
+                () => {
+                  router.push("/");
+                };
               }}
             >
               signout
@@ -77,20 +116,46 @@ useEffect(()=>{
         </div>
         <div ref={big} className="flex justify-center mt-64 text-center">
           <div className="gap-y-10">
-            <motion.p style={{scale}} className="font-medium">Welcome to SIDE+</motion.p>
-            <motion.p style={{scale}} className="mt-6 text-5xl font-semibold tracking-wider">
+            <motion.p style={{ scale }} className="font-medium">
+              Welcome to SIDE+
+            </motion.p>
+            <motion.p
+              style={{ scale }}
+              className="mt-6 text-5xl font-semibold tracking-wider"
+            >
               WATCH EVERTHING HERE!
             </motion.p>
-            <motion.button style={{scale}} className="p-2 px-8 mt-4 text-xl font-semibold rounded-md button">
+            <motion.button
+              style={{ scale }}
+              className="p-2 px-8 mt-4 text-xl font-semibold rounded-md button"
+            >
               Watch now
             </motion.button>
           </div>
         </div>
       </div>
-      <motion.div ref={ref} style={{marginTop:padding}}>
-      <Second images={images} prev={true} buttonText="Join to Watch" image={image}></Second>
-      <Second images={images} prev={false} buttonText="See all" image={BTS}></Second>
-      <Second images={images} prev={false} buttonText="See all loyalty rewards" image={Third}></Second>
+      <motion.div ref={ref} style={{ marginTop: padding }}>
+        <Second
+          images={images}
+          prev={true}
+          buttonText="Join to Watch"
+          image={image}
+          side="Exclisive series just for side+ members"
+        ></Second>
+        <Second
+          images={images2}
+          prev={false}
+          buttonText="See all"
+          image={BTS}
+          side="Uncut and uncensored footage"
+        ></Second>
+        <Second
+          images={images3}
+          prev={false}
+          buttonText="See all loyalty rewards"
+          image={Third}
+          side="We like to give back to you guys"
+        ></Second>
       </motion.div>
     </div>
   );
