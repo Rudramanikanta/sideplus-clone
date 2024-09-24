506860631641a833304c2e28126b2965dac93102
rudramanikanta <rudramanikanta02@gmail.com>
2024-09-24 21:09:12 +0530

    bottom component update

--- app/components/Bottom.js
+++ app/components/Bottom.js
@@ -5,9 +5,9 @@ const Bottom = () => {
     <div className='bg-[#141414] h-[50vh] block'>
         <div className='p-10 md:flex'>
             <div>
-                <p>Latest News</p>
-                <p>Chuck your emial in here to stay up to date with all things Sidemen, direct to your inboc</p>
-                <input className='bg-neutral-600/10'></input>
+                <p className='text-white text-md'>Latest News</p>
+                <p className='mt-3 text-sm text-neutral-400 '>Chuck your emial in here to stay up to date with all things Sidemen, direct to your inboc</p>
+                <input className='p-3 mt-2 bg-neutral-600/10 hover:border-2 hover:bg-blue-700 ' placeholder='send email'></input>
             </div>
            
         </div>
